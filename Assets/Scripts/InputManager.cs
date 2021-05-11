a39ca29c5423bff1ac88f96df296938e82f62e9e
Francesco <francesco.testa.ge@gmail.com>
2021-05-11 17:16:57 +0200

    UIholograms resize implementation, Added a better user interface

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -6,16 +6,36 @@ using UnityEngine.Windows.Speech;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using Microsoft.MixedReality.Toolkit.UI;
+using TMPro;
 
 public class InputManager : MonoBehaviour
 {
+    #region Public Variables
+    [SerializeField]
+    public TextMeshPro textObj = null;
+
+    #endregion
+
+
     #region Member Variables
+    // Istance of the map to resize it  
     private GameObject suMinimap = null;
-    #endregion // Member Variables
+    // Istance of te UI object to resize it as well as the map
+    private GameObject UIobject = null;
+
+    private Transform mapFrame;
+
+    private float scale = 0.1f;
+    #endregion 
 
     [Tooltip("Reference to the main scene understanding manager for default commands.")]
     [SerializeField]
     private MySUManager suManager;
+
+    [Tooltip("Reference to the scene object placer to handle the placed object")]
+    [SerializeField]
+    private SceneUnderstandingObjectPlacer SceneObjPlacer;
 
     [Tooltip("Reference to the Labeler Component for SU Scene")]
     [SerializeField]
@@ -23,6 +43,7 @@ public class InputManager : MonoBehaviour
 
     private bool toggleMiniMap = false;
 
+    #region Toggle Map Features
     public async void enableMeshWorld()
     {
         suManager.RenderWorldMesh = !suManager.RenderWorldMesh;
@@ -40,20 +61,51 @@ public class InputManager : MonoBehaviour
         suManager.RenderSceneObjects = !suManager.RenderSceneObjects;
         await suManager.DisplayDataAsync();
     }
+    #endregion
 
- 
-    /// <summary>
-    /// Turns the mini map off.
-    /// </summary>
-    private void MiniMapOff()
+    public void OnSliderUpdated(SliderEventData eventData)
     {
-        if (suMinimap != null)
-        {
-            DestroyImmediate(suMinimap);
-            suMinimap = null;
-        }
-        suManager.SceneRoot.SetActive(true);
+        scale = eventData.NewValue;
+        textObj.text = $"{scale:F2}";
+
     }
+
+    #region UI object resize
+    private void resizeUIObject()
+    {
+        if (UIobject == null)
+        {
+            UIobject = Instantiate(SceneObjPlacer.objectPlaced);
+            // riportare la posizione dell'oggetto rispetto a questo frame.
+
+            // setto il teso
+            //UIobject.transform.position = Camera.main.transform.position + Camera.main.transform.forward;
+            UIobject.transform.position = new Vector3(0, 0, 0);
+            UIobject.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+            SceneObjPlacer.objectPlaced.SetActive(false);
+        }
+    }
+
+    private void addUIobjects()
+    {
+        UIobject = Instantiate(SceneObjPlacer.objectPlaced);
+        UIobject.transform.parent = suMinimap.transform;
+        SceneObjPlacer.objectPlaced.SetActive(false);
+    }
+
+
+    private void unresizeUIObject()
+    {
+        if(UIobject != null)
+        {
+            DestroyImmediate(UIobject);
+            UIobject = null;
+        }
+        SceneObjPlacer.objectPlaced.SetActive(true);
+    }
+    #endregion
+
+    #region Map Resize
 
     /// <summary>
     /// Turns the mini map on.
@@ -64,12 +116,30 @@ public class InputManager : MonoBehaviour
         {
             suMinimap = Instantiate(suManager.SceneRoot);
             suMinimap.name = "Minimap";
+            
+            addUIobjects();
             suMinimap.transform.position = Camera.main.transform.position + Camera.main.transform.forward;
-            suMinimap.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-
+            suMinimap.transform.localScale = new Vector3(scale, scale, scale);
             suManager.SceneRoot.SetActive(false);
+
         }
     }
+
+    /// <summary>
+    /// Turns the mini map off.
+    /// </summary>
+    private void MiniMapOff()
+    {
+        if (suMinimap != null)
+        {
+            DestroyImmediate(suMinimap);
+            suMinimap = null;
+        }
+        // destroy all objects
+        unresizeUIObject();
+        suManager.SceneRoot.SetActive(true);
+    }
+
 
     /// <summary>
     /// Toggles the mini map on and off.
@@ -80,10 +150,14 @@ public class InputManager : MonoBehaviour
         if (toggleMiniMap)
         {
             MiniMapOn();
+            //Resize UISceneObject 
+            //resizeUIObject();
         }
         else
         {
             MiniMapOff();
+            //unresizeUIObject();
         }
     }
+    #endregion
 }
